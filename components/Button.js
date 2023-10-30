2f7eae07653651073e568b10f7c5de75ba57883d
Delesoo <strongtoka@gmail.com>
2023-10-30 15:07:23 +0400

    databases

--- components/Button.js
+++ components/Button.js
@@ -1,37 +1,42 @@
 import styled, { css } from "styled-components";
 
-const StyledButton = styled.button`
-    border:0;
-    padding: 5px 15px;
-    border-radius: 5px;
-    cursor:pointer;
-    display: inline-flex;
-    align-items: center;
+export const ButtonStyle = css`
+border:0;
+padding: 5px 15px;
+border-radius: 5px;
+cursor:pointer;
+display: inline-flex;
+align-items: center;
+text-decoration: none;
+svg{
+    height: 16px;
+    margin-right: 5px;
+}
+${props => props.white && !props.outline && css`
+    background-color: #fff;
+    color: #000;
+`}
+${props => props.white && props.outline && css`
+    background-color: transparent;
+    color: #fff;
+    border: 1px solid #fff;
+`}
+${props => props.primary && css`
+    background-color: #5542F6;
+    border: 1px solid #5542F6;
+    color: #fff;
+`}
+${props => props.size === 'l' && css`
+    font-size: 1.2rem;
+    padding: 10px 20px;
     svg{
-        height: 16px;
-        margin-right: 5px;
+        height: 20px;
     }
-    ${props => props.white && !props.outline && css`
-        background-color: #fff;
-        color: #000;
-    `}
-    ${props => props.white && props.outline && css`
-        background-color: transparent;
-        color: #fff;
-        border: 1px solid #fff;
-    `}
-    ${props => props.primary && css`
-        background-color: #5542F6;
-        border: 1px solid #5542F6;
-        color: #fff;
-    `}
-    ${props => props.size === 'l' && css`
-        font-size: 1.2rem;
-        padding: 10px 20px;
-        svg{
-            height: 20px;
-        }
-    `}
+`}
+`;
+
+const StyledButton = styled.button`
+    ${ButtonStyle}
 `;
 
 export default function Button({children,...rest}) {
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Products";
 
-export default function HomePage() {
+export default function HomePage({product}) {
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product} />
     </div>
   );
 }
 
-export function getServerSideProps () {
-  
+export async function getServerSideProps () {
+  const featuredProductId = '653b743eef19534ae3264634';
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: {product: JSON.parse(JSON.stringify(product))},
+  }
 }
